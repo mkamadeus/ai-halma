7730389017fd23998577d4065bef70ff40225cbf
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-10-21 03:54:32 +0700

    Setting page done

--- src/Setting.js
+++ src/Setting.js
@@ -4,16 +4,9 @@ import { useHistory } from "react-router-dom";
 const Setting = () => {
   const [playerBlue, setPlayerBlue] = useState("human");
   const [playerOrange, setPlayerOrange] = useState("human");
-  const [bSize, setBSize] = useState(4);
+  const [bSize, setBSize] = useState(6);
   const [tLimit, setTLimit] = useState(10);
   const history = useHistory();
-
-  const handleChange = (event) => {
-    setPlayerBlue(event.target.playerBlue);
-    setPlayerOrange(event.target.playerOrange);
-    setBSize(event.target.bSize);
-    setTLimit(event.target.tLimit);
-  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -21,58 +14,66 @@ const Setting = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Blue player:
+    <form onSubmit={handleSubmit} className="flex flex-wrap p-3">
+      <div className="px-3">
+        <label className="pr-2">Blue player:</label>
         <select
           value={playerBlue}
           onChange={(event) => {
             setPlayerBlue(event.target.value);
           }}
+          className="p-2 border border-gray-400"
         >
           <option value="human">Human</option>
           <option value="minimax">AI - Minimax</option>
           <option value="minimaxlocal">AI - Minimax + Local Search</option>
         </select>
-      </label>
-      <label>
-        Orange player:
+      </div>
+      <div className="px-3">
+        <label className="pr-2">Orange player:</label>
         <select
           value={playerOrange}
           onChange={(event) => {
             setPlayerOrange(event.target.value);
           }}
+          className="p-2 border border-gray-400"
         >
           <option value="human">Human</option>
           <option value="minimax">AI - Minimax</option>
           <option value="minimaxlocal">AI - Minimax + Local Search</option>
         </select>
-      </label>
-      <label>
-        Board size:
+      </div>
+      <div className="px-3">
+        <label className="pr-2">Board size:</label>
         <select
           value={bSize}
           onChange={(event) => {
             setBSize(event.target.value);
           }}
+          className="p-2 border border-gray-400"
         >
           <option value={6}>6</option>
           <option value={8}>8</option>
           <option value={10}>10</option>
           <option value={16}>16</option>
         </select>
-      </label>
-      <label>
-        Time limit for each turn (in seconds):
+      </div>
+      <div className="px-3">
+        <label className="pr-2">Time limit for each turn (in seconds):</label>
         <input
           type="number"
           value={tLimit}
           onChange={(event) => {
-            setTLimit(event.target.value);
+            setTLimit(parseInt(event.target.value));
           }}
+          className="p-2 border border-gray-400"
         />
-      </label>
-      <input type="submit" value="Start" />
+      </div>
+      <div className="px-3">
+        <button type="submit" className="p-2 bg-blue-400 text-white rounded">
+          Let's go!
+        </button>
+      </div>
     </form>
   );
 };
